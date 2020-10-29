6bbaf15a2c0da80411a37d4e7e12918de68316a6
Kevin Shook <kevin.shook@usask.ca>
2020-10-29 10:32:57 -0600

    added package file

---
+++ R/bootstrappR-package.R
@@ -0,0 +1,19 @@
+#' bootstrappR: Complete Stochastic Modelling Solution
+#'
+#' bootstrappR is an R package which does bootstrap and jackknife analyses of the error in hydrological models by
+#' estimating the empirical probability distributions of NSE and KGE estimoors.
+#'
+#' @author \strong{Coded by:} Kevin Shook \email{kevin.shook@usask.ca} and...
+#' @author \strong{Conceptual design by:} Martyn Clark \email{martyn.clark@usask.ca}
+#' @author \strong{Maintained by:} Guoqiang Tang \email{guoqiang.tang@usask.ca}
+#'
+#' @section Funding:
+#'
+#' The package was partly funded by the Global institute for Water Security (GIWS; \href{https://www.usask.ca/water/}{https://www.usask.ca/water/}) and the Global Water Futures (GWF; \href{https://gwf.usask.ca/}{https://gwf.usask.ca/}) program.
+#'
+#' @references Clark, M.P, et al. 2020. "Evaluation of global performance metrics habitually used in hydrological modelling studies: A critique and path forward"
+#'
+#' @docType package
+#' @name bootstrappR-package
+NULL
+#> NULL
