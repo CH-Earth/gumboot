3db5226e623e00ebdad4a08ad18dbf7dfb03de58
Kevin Shook <kevin.shook@usask.ca>
2021-03-21 20:09:25 -0600

    added flows_1030500 and fixed a few minor bugs

---
+++ R/flows_1030500.R
@@ -0,0 +1,12 @@
+#' Observed and simulated flows for a single location
+#'
+#' @description A data frame observed and simulated flows for USGS site 1030500
+#'
+#' @format A data frame with 6940 rows and 3 variables:
+#' \describe{
+#'   \item{date}{Date of flows)}
+#'   \item{obs}{observed flows (m\eqn{^3}{^3})/s)}
+#'   \item{sim}{simulated flows (m\eqn{^3}{^3})/s)}
+#' }
+
+"flows_1030500"
